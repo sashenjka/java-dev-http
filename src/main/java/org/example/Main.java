603979afe8d64b386efac71e8ff2a94cad759cb2
Oleksandra <oleksandrastratijcuk@gmail.com>
2023-10-13 14:48:35 +0300

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import org.example.http.HttpImageStatusCli;
+public class Main {
+    public static void main(String[] args) {
+        HttpImageStatusCli cli = new HttpImageStatusCli();
+        cli.askStatus();
+    }
+}
